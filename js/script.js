e041162c00f0d45e5c020621ae89d100afabad6e
Vedmedk0 <Vedmedk0@mail.ru>
2020-01-17 23:00:15 +0300

    cv prototype

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-var a = 5
-console.log(a)
+var a = 5;
+
+
+$(document).ready(function(){
+    $('.page').hide();
+    $('#aboutme').show();
+
+    $('.nav-link').click(
+        function(event){
+            event.preventDefault();
+            $('.page').hide();
+            $('#'+$(this).attr('data-page')).show();
+        }
+    );
+
+})
+console.log(a);
